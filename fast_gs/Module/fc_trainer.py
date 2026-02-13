02831357507f3346bfa68057b08bbf9ea9cf4240
chLi <565353780@qq.com>
2026-02-13 14:33:08 +0800

    move common script into base gs trainer

--- fast_gs/Module/fc_trainer.py
+++ fast_gs/Module/fc_trainer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.append('../../../base-gs-trainer/')
 import torch
 
 from torch import nn
@@ -9,8 +10,7 @@ from argparse import ArgumentParser
 
 from fused_ssim import fused_ssim
 
-from base_trainer.Module.logger import Logger
-from base_trainer.Module.base_trainer import BaseTrainer
+from base_gs_trainer.Module.base_gs_trainer import BaseGSTrainer
 
 from camera_control.Module.nvdiffrast_renderer import NVDiffRastRenderer
 
@@ -24,37 +24,24 @@ else:
     from fast_gs.Lib.chamfer3D.chamfer_python import distChamfer
 
 from fast_gs.Config.config import ModelParams, PipelineParams, OptimizationParams
-from fast_gs.Lib.lpipsPyTorch import lpips
 from fast_gs.Loss.l1 import l1_loss
-from fast_gs.Metric.psnr import psnr
-from fast_gs.Dataset.scene import Scene
 from fast_gs.Model.gs import GaussianModel
-from fast_gs.Method.general_utils import safe_state
 from fast_gs.Method.render_kernel import render_fastgs
 from fast_gs.Method.fast_utils import compute_gaussian_score_fastgs, sampling_cameras
 
 
-class FCTrainer(object):
+class FCTrainer(BaseGSTrainer):
     def __init__(
         self,
         colmap_data_folder_path: str='',
         init_mesh_file_path: str='',
+        device: str='cuda:0',
         save_result_folder_path: str='./output/',
         save_log_folder_path: str='./logs/',
-        device: str='cuda:0',
         test_freq: int=10000,
         save_freq: int=10000,
         fc_update_freq: int=100,
     ) -> None:
-        # 优先设定默认 dtype，避免其他脚本设为 bfloat16 导致类型不匹配
-        torch.set_default_dtype(torch.float32)
-
-        self.save_result_folder_path = save_result_folder_path
-        self.save_log_folder_path = save_log_folder_path
-        self.device = device
-
-        self.test_freq = test_freq
-        self.save_freq = save_freq
         self.fc_update_freq = fc_update_freq
 
         # Set up command line argument parser
@@ -69,23 +56,11 @@ class FCTrainer(object):
 
         print("Optimizing " + args.model_path)
 
-        # Initialize system state (RNG)
-        safe_state(silent=False)
-
-        torch.autograd.set_detect_anomaly(False)
-
-        os.makedirs(args.model_path, exist_ok=True)
-
         self.dataset = lp.extract(args)
         self.opt = op.extract(args)
         self.pipe = pp.extract(args)
 
         self.gaussians = GaussianModel(self.dataset.sh_degree)
-        self.scene = Scene(self.dataset, self.gaussians)
-        self.gaussians.training_setup(self.opt)
-
-        bg_color = [1, 1, 1] if self.dataset.white_background else [0, 0, 0]
-        self.background = torch.tensor(bg_color, dtype=torch.float32, device=self.device)
 
         assert os.path.exists(init_mesh_file_path)
         self.fc_params = FCConvertor.createFC(
@@ -112,12 +87,15 @@ class FCTrainer(object):
 
         self.E_thinplate_base = None
 
-        self.logger = Logger()
-
-        BaseTrainer.initRecords(self)
-
-        # tmp
-        self.is_gt_logged = False
+        BaseGSTrainer.__init__(
+            self,
+            colmap_data_folder_path=colmap_data_folder_path,
+            device=device,
+            save_result_folder_path=save_result_folder_path,
+            save_log_folder_path=save_log_folder_path,
+            test_freq=test_freq,
+            save_freq=save_freq,
+        )
         return
 
     def renderImage(self, viewpoint_cam) -> dict:
@@ -224,55 +202,6 @@ class FCTrainer(object):
         }
 
         return render_pkg, loss_dict
-
-    @torch.no_grad
-    def logStep(self, iteration: int, loss_dict: dict) -> bool:
-        for key, value in loss_dict.items():
-            self.logger.addScalar('Loss/' + key, value, iteration)
-
-        self.logger.addScalar('Gaussian/total_points', self.gaussians.get_xyz.shape[0], iteration)
-        self.logger.addScalar('Gaussian/scale', torch.mean(self.gaussians.get_scaling).detach().clone().cpu().numpy(), iteration)
-        self.logger.addScalar('Gaussian/opacity', torch.mean(self.gaussians.get_opacity).detach().clone().cpu().numpy(), iteration)
-
-        # Report test and samples of training set
-        if iteration % self.test_freq == 0:
-            torch.cuda.empty_cache()
-            config = {'name': 'train', 'cameras' : [self.scene[idx % len(self.scene)] for idx in range(5, 30, 5)]}
-            if config['cameras'] and len(config['cameras']) > 0:
-                l1_test = 0.0
-                psnr_test, ssim_test, lpips_test = 0.0, 0.0, 0.0
-                for idx, viewpoint in enumerate(config['cameras']):
-                    render_pkg = self.renderImage(viewpoint)
-                    image = torch.clamp(render_pkg["render"], 0.0, 1.0)
-                    gt_image = torch.clamp(viewpoint.original_image.to("cuda"), 0.0, 1.0)
-
-                    fc_mesh = self.extractMesh()[0]
-                    normal_image = torch.clamp(NVDiffRastRenderer.renderNormal(fc_mesh, viewpoint._cam)['normal_camera'], 0.0, 1.0).permute(2, 0, 1)
-                    if self.logger.isValid() and (idx < 5):
-                        self.logger.summary_writer.add_images(config['name'] + "_view_{}/render".format(viewpoint.image_name), image[None], global_step=iteration)
-                        self.logger.summary_writer.add_images(config['name'] + "_view_{}/fc_normal".format(viewpoint.image_name), normal_image[None], global_step=iteration)
-
-                        if not self.is_gt_logged:
-                            self.logger.summary_writer.add_images(config['name'] + "_view_{}/ground_truth".format(viewpoint.image_name), gt_image[None], global_step=iteration)
-                            self.is_gt_logged = True
-
-                    l1_test += l1_loss(image, gt_image).mean().double()
-                    psnr_test += psnr(image, gt_image).mean().double()
-                    ssim_test += fused_ssim(image.unsqueeze(0), gt_image.unsqueeze(0)).mean().double()
-                    lpips_test += lpips(image, gt_image, net_type='vgg').mean().double()
-
-                psnr_test /= len(config['cameras'])
-                ssim_test /= len(config['cameras'])
-                lpips_test /= len(config['cameras'])
-                l1_test /= len(config['cameras'])
-                print("\n[ITER {}] Evaluating {}: L1 {} PSNR {}".format(iteration, config['name'], l1_test, psnr_test))
-                self.logger.addScalar('Eval/l1', l1_test, iteration)
-                self.logger.addScalar('Eval/psnr', psnr_test, iteration)
-                self.logger.addScalar('Eval/ssim', ssim_test, iteration)
-                self.logger.addScalar('Eval/lpips', lpips_test, iteration)
-
-            torch.cuda.empty_cache()
-        return True
 
     @torch.no_grad()
     def recordGrads(self, render_pkg: dict) -> bool:
