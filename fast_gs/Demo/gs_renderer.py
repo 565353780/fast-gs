e8cef4cb4787d390b8fd02b4fa2d75f29296029f
chLi <565353780@qq.com>
2026-03-20 15:44:50 +0800

    finish test render with standard camera poses

--- fast_gs/Demo/gs_renderer.py
+++ fast_gs/Demo/gs_renderer.py
@@ -2,15 +2,21 @@ import sys
 sys.path.append('../base-trainer')
 sys.path.append('../base-gs-trainer')
 sys.path.append('../camera-control')
+sys.path.append('../x-flux-3d-mv')
 
 import os
 os.environ['CUDA_VISIBLE_DEVICES']='6'
 
 import cv2
 import numpy as np
+import open3d as o3d
+
+from math import tan
 
 from camera_control.Module.camera_convertor import CameraConvertor
 from camera_control.Module.camera_filter import CameraFilter
+
+from flux_mv.Dataset.mesh import fovToCameraDist
 
 from fast_gs.Module.gs_renderer import GSRenderer
 
@@ -24,13 +30,50 @@ def demo():
     save_render_folder_path = home + '/chLi/Dataset/GS/' + data_id + '/fastgs_render/'
     os.makedirs(save_render_folder_path, exist_ok=True)
 
+    pcd = o3d.io.read_point_cloud(gs_ply_file_path)
+    pts = np.asarray(pcd.points)  # Nx3
+
+    bbox_min = pts.min(axis=0)
+    bbox_max = pts.max(axis=0)
+    bbox_center = (bbox_min + bbox_max) / 2
+
+    print("bbox min:", bbox_min)
+    print("bbox max:", bbox_max)
+    print("bbox center:", bbox_center)
+
     camera_list = CameraConvertor.loadColmapDataFolder(colmap_data_folder_path)
 
-    fps_camera_idxs = CameraFilter.sampleFarCameraIdxs(camera_list, sample_camera_num=10)
+    width = height = 784
+    fov_degree = 60.0
+    target_position = [0, 0, 0]
+    target_position = bbox_center
+    camera_dist = fovToCameraDist(fov_degree, margin=1.0)
+    fovx = fovy = np.radians(fov_degree)
+
+    fps_camera_idxs = CameraFilter.samplePolarFarCameraIdxs(
+        camera_list=camera_list,
+        target_position=target_position,
+        sample_camera_num=10,
+    )
     fps_camera_list = [camera_list[idx] for idx in fps_camera_idxs]
 
-    for fps_camera in fps_camera_list:
-        fps_camera.setImageSize(784, 784)
+    for i, fps_camera in enumerate(fps_camera_list):
+        fps_camera.setImageSize(width, height)
+        fps_camera.fx = fps_camera.width / 2.0 / tan(fovx / 2.0)
+        fps_camera.fy = fps_camera.height / 2.0 / tan(fovy / 2.0)
+
+        polar = fps_camera.toPolar(target_position)
+        phi, theta = polar[0], polar[1]
+        direction = np.array([
+            np.sin(phi) * np.sin(theta),
+            np.sin(phi) * np.cos(theta),
+            np.cos(phi),
+        ])
+        new_pos = np.array(target_position) + direction * camera_dist
+        fps_camera.setWorldPose(look_at=target_position, pos=new_pos, up=[0, 1, 0])
+
+        uv = fps_camera.project_points_to_uv(target_position)
+        print(f"Camera {i}: target uv = ({uv[0, 0].item():.4f}, {uv[0, 1].item():.4f})")
 
     render_list = GSRenderer.renderCameras(
         gs_ply_file_path,
